4ab30acf3424f42dc2f8851e2e71255bd26da409
Alpacorp <alejandro.palacios88@gmail.com>
2024-02-25 23:07:29 -0500

    feat: add metadata to signup page

--- src/app/(auth)/signup/layout.tsx
+++ src/app/(auth)/signup/layout.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Sign Up",
+  description: "Sign up for an account",
+};
+
 export default function SignUpLayout({
   children,
 }: Readonly<{
